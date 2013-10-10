b99125f14694c5501987f6df1f8d2f36efb8a49b
Makoto YUI <yuin405@gmail.com>
2013-10-10 23:38:12 +0900

    Fixed style of license header

--- src/main/hivemall/tools/macros/SigmodUDF.java
+++ src/main/hivemall/tools/macros/SigmodUDF.java
@@ -1,14 +1,14 @@
-/**
+/*
  * Hivemall: Hive scalable Machine Learning Library
  *
  * Copyright (C) 2013
  *   National Institute of Advanced Industrial Science and Technology (AIST)
  *   Registration Number: H25PRO-1520
- *   
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
