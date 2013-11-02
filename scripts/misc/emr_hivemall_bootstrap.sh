a22a347fdecb1ed37257878efd8ed5eab470af42
Makoto YUI <yuin405@gmail.com>
2013-11-03 03:55:28 +0900

    bug fixed

--- scripts/misc/emr_hivemall_bootstrap.sh
+++ scripts/misc/emr_hivemall_bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir tmp
-wget --no-check-certificate -P ./tmp \
+mkdir -p /home/hadoop/tmp
+wget --no-check-certificate -P /home/hadoop/tmp \
  https://github.com/myui/hivemall/raw/master/target/hivemall.jar https://github.com/myui/hivemall/raw/master/scripts/ddl/define-all.hive
