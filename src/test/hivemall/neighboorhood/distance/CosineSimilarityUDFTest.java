2a1fcdbc1a9d0d1e45090cdaa1ea5f032e7008e0
Makoto YUI <yuin405@gmail.com>
2014-04-24 17:39:19 +0900

    Fixed the package of CosineSimilarityUDF

--- src/test/hivemall/neighboorhood/distance/CosineSimilarityUDFTest.java
+++ src/test/hivemall/neighboorhood/distance/CosineSimilarityUDFTest.java
@@ -18,7 +18,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package hivemall.ftvec.distance;
+package hivemall.neighboorhood.distance;
+
+import hivemall.neighborhood.distance.CosineSimilarityUDF;
 
 import java.util.Arrays;
 import java.util.List;
