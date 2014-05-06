d73f562e4faba8d0d803b546fc6ba3d41d5036e0
Makoto YUI <yuin405@gmail.com>
2014-05-06 13:24:18 +0900

    Add jaccard index UDF

---
+++ src/main/hivemall/neighborhood/distance/JaccardIndexUDF.java
@@ -0,0 +1,37 @@
+/*
+ * Hivemall: Hive scalable Machine Learning Library
+ *
+ * Copyright (C) 2013-2014
+ *   National Institute of Advanced Industrial Science and Technology (AIST)
+ *   Registration Number: H25PRO-1520
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package hivemall.neighborhood.distance;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public final class JaccardIndexUDF extends UDF {
+
+    public float evaluate(long a, long b) {
+        return evaluate(a, b, 128);
+    }
+
+    public float evaluate(long a, long b, int k) {
+        int countMatches = k - HammingDistanceUDF.hammingDistance(a, b);
+        float jaccard = countMatches / k;
+        return 2.f * (jaccard - 0.5f);
+    }
+
+}
