84576ecdd957caee1bc8443abc3732c2218e310b
Makoto YUI <yuin405@gmail.com>
2014-06-25 10:56:20 +0900

    Moved hivemall.tools.macros.SigmodUDF to hivemall.tools.math.SigmodUDF

--- src/main/hivemall/tools/math/SigmodUDF.java
+++ src/main/hivemall/tools/math/SigmodUDF.java
@@ -18,7 +18,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package hivemall.tools.macros;
+package hivemall.tools.math;
 
 import static hivemall.utils.WritableUtils.val;
 
