736ab241490a02b625a044acd74da998709ab249
Makoto YUI <yuin405@gmail.com>
2014-07-16 22:17:15 +0900

    Add NumberUtils

---
+++ src/main/hivemall/utils/lang/NumberUtils.java
@@ -0,0 +1,35 @@
+package hivemall.utils.lang;
+
+public final class NumberUtils {
+
+    private NumberUtils() {}
+
+    public static int parseInt(String s) {
+        int endIndex = s.length() - 1;
+        char last = s.charAt(endIndex);
+        if(Character.isLetter(last)) {
+            String numstr = s.substring(0, endIndex);
+            int i = Integer.parseInt(numstr);
+            switch(last) {
+                case 'k':
+                case 'K':
+                    i *= 1000;
+                    break;
+                case 'm':
+                case 'M':
+                    i *= 1000000;
+                    break;
+                case 'g':
+                case 'G':
+                    i *= 1000000000;
+                    break;
+                default:
+                    throw new NumberFormatException("Invalid number format: " + s);
+            }
+            return i;
+        } else {
+            return Integer.parseInt(s);
+        }
+    }
+
+}
