e2cc31cfd6c2ab6def8267404dc82060e3311131
Makoto YUI <yuin405@gmail.com>
2014-08-27 14:59:14 +0900

    Added UDTFWithOptions#parseFeatures

--- src/main/hivemall/UDTFWithOptions.java
+++ src/main/hivemall/UDTFWithOptions.java
@@ -20,13 +20,22 @@
  */
 package hivemall;
 
+import hivemall.io.FeatureValue;
+import hivemall.utils.hadoop.WritableUtils;
 import hivemall.utils.lang.CommandLineUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils.ObjectInspectorCopyOption;
+import org.apache.hadoop.io.Writable;
 
 public abstract class UDTFWithOptions extends GenericUDTF {
 
@@ -40,5 +49,28 @@ public abstract class UDTFWithOptions extends GenericUDTF {
 
     protected abstract CommandLine processOptions(ObjectInspector[] argOIs)
             throws UDFArgumentException;
+
+    protected List<FeatureValue> parseFeatures(final List<?> features, final ObjectInspector featureInspector, final boolean parseFeature) {
+        final int numFeatures = features.size();
+        if(numFeatures == 0) {
+            return Collections.emptyList();
+        }
+        final List<FeatureValue> list = new ArrayList<FeatureValue>(numFeatures);
+        for(Object f : features) {
+            if(f == null) {
+                continue;
+            }
+            final FeatureValue fv;
+            if(parseFeature) {
+                fv = FeatureValue.parse(f);
+            } else {
+                Object o = ObjectInspectorUtils.copyToStandardObject(f, featureInspector, ObjectInspectorCopyOption.WRITABLE);
+                Writable k = WritableUtils.toWritable(o);
+                fv = new FeatureValue(k, 1.f);
+            }
+            list.add(fv);
+        }
+        return list;
+    }
 
 }
