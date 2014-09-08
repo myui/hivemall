abd733f1065b64587c7aa6598b8e460fb7547f94
Makoto YUI <yuin405@gmail.com>
2014-09-08 22:56:10 +0900

    Simplified implementation

--- src/main/hivemall/regression/LogressUDTF.java
+++ src/main/hivemall/regression/LogressUDTF.java
@@ -21,9 +21,7 @@
 package hivemall.regression;
 
 import hivemall.common.EtaEstimator;
-import hivemall.common.EtaEstimator.InvscalingEtaEstimator;
-import hivemall.common.EtaEstimator.SimpleEtaEstimator;
-import hivemall.utils.math.MathUtils;
+import hivemall.common.LossFunctions;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
@@ -31,7 +29,7 @@ import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
 
-public class LogressUDTF extends OnlineRegressionUDTF {
+public final class LogressUDTF extends OnlineRegressionUDTF {
 
     private EtaEstimator etaEstimator;
 
@@ -57,53 +55,23 @@ public class LogressUDTF extends OnlineRegressionUDTF {
     @Override
     protected CommandLine processOptions(ObjectInspector[] argOIs) throws UDFArgumentException {
         CommandLine cl = super.processOptions(argOIs);
-
-        this.etaEstimator = getEtaEstimator(cl);
+        
+        this.etaEstimator = EtaEstimator.get(cl);
         return cl;
     }
 
-    protected static EtaEstimator getEtaEstimator(CommandLine cl) throws UDFArgumentException {
-        if(cl == null) {
-            return new InvscalingEtaEstimator(0.1f, 0.1f);
-        }
-
-        float eta0 = Float.parseFloat(cl.getOptionValue("eta0", "0.1"));
-
-        if(cl.hasOption("t")) {
-            int t = Integer.parseInt(cl.getOptionValue("t"));
-            return new SimpleEtaEstimator(eta0, t);
-        }
-
-        float power_t = Float.parseFloat(cl.getOptionValue("power_t", "0.1"));
-        return new InvscalingEtaEstimator(eta0, power_t);
-    }
-
     @Override
-    protected void checkTargetValue(float target) throws UDFArgumentException {
+    protected void checkTargetValue(final float target) throws UDFArgumentException {
         if(target < 0.f || target > 1.f) {
             throw new UDFArgumentException("target must be in range 0 to 1: " + target);
         }
     }
 
     @Override
-    protected float dloss(float target, float predicted) {
-        float eta = eta();
-        float loss = computeLoss(target, predicted);
-        return eta * loss;
-    }
-
-    private float eta() {
-        return etaEstimator.eta(count);
-    }
-
-    private static float computeLoss(float target, float predicted) {
-        final float gradient;
-        if(-100.d < predicted) {
-            gradient = target - (float) MathUtils.sigmoid(predicted);
-        } else {
-            gradient = target;
-        }
-        return gradient;
+    protected float computeUpdate(final float target, final float predicted) {
+        float eta = etaEstimator.eta(count);
+        float gradient = LossFunctions.logisticLoss(target, predicted);
+        return eta * gradient;
     }
 
 }
