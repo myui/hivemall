ae493a23801ae5fd3de9158d799587de5887d273
myui <yuin405@gmail.com>
2015-05-05 02:09:32 +0900

    Added hivemall_version() UDF

--- src/main/java/hivemall/HivemallConstants.java
+++ src/main/java/hivemall/HivemallConstants.java
@@ -20,6 +20,8 @@ package hivemall;
 
 public final class HivemallConstants {
 
+    public static final String VERSION = "0.3.1";
+
     public static final String BIAS_CLAUSE = "0";
     public static final String CONFKEY_RAND_AMPLIFY_SEED = "hivemall.amplify.seed";
 
