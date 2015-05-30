9f7f0c81fb38801152c9738c72413113619b924a
myui <yuin405@gmail.com>
2015-05-29 19:15:24 -0700

    Fixed a compilation error

--- src/test/java/hivemall/knn/distance/EuclidDistanceUDFTest.java
+++ src/test/java/hivemall/knn/distance/EuclidDistanceUDFTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.apache.hadoop.hive.serde2.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,8 +13,8 @@ public class EuclidDistanceUDFTest {
     @Test
     public void test1() {
         EuclidDistanceUDF udf = new EuclidDistanceUDF();
-        List<String> ftvec1 = Arrays.asList("1:1.0", "2:2.0", "3:3.0");
-        List<String> ftvec2 = Arrays.asList("1:2.0", "2:4.0", "3:6.0");
+        List<Text> ftvec1 = Arrays.asList(new Text("1:1.0"), new Text("2:2.0"), new Text("3:3.0"));
+        List<Text> ftvec2 = Arrays.asList(new Text("1:2.0"), new Text("2:4.0"), new Text("3:6.0"));
         DoubleWritable d = udf.evaluate(ftvec1, ftvec2);
         Assert.assertEquals(Math.sqrt(1.0 + 4.0 + 9.0), d.get(), 0.d);
     }
@@ -21,8 +22,8 @@ public class EuclidDistanceUDFTest {
     @Test
     public void test2() {
         EuclidDistanceUDF udf = new EuclidDistanceUDF();
-        List<String> ftvec1 = Arrays.asList("1:1.0", "2:3.0", "3:3.0");
-        List<String> ftvec2 = Arrays.asList("1:2.0", "3:6.0");
+        List<Text> ftvec1 = Arrays.asList(new Text("1:1.0"), new Text("2:3.0"), new Text("3:3.0"));
+        List<Text> ftvec2 = Arrays.asList(new Text("1:2.0"), new Text("3:6.0"));
         DoubleWritable d = udf.evaluate(ftvec1, ftvec2);
         Assert.assertEquals(Math.sqrt(1.0 + 9.0 + 9.0), d.get(), 0.d);
     }
