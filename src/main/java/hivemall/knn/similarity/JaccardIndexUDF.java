ea5e0895772a3130bb2b20cdbbc79d9114fba7cb
myui <yuin405@gmail.com>
2015-06-17 17:53:32 +0900

    Updated jaccard to accept arrays as the inputs

--- src/main/java/hivemall/knn/similarity/JaccardIndexUDF.java
+++ src/main/java/hivemall/knn/similarity/JaccardIndexUDF.java
@@ -22,13 +22,21 @@ import static hivemall.utils.hadoop.WritableUtils.val;
 import hivemall.knn.distance.HammingDistanceUDF;
 
 import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.hive.ql.udf.UDFType;
 import org.apache.hadoop.io.FloatWritable;
 
+@Description(name = "jaccard", value = "_FUNC_(A, B [,int k]) - Returns Jaccard similarity coefficient of A and B")
 @UDFType(deterministic = true, stateful = false)
 public final class JaccardIndexUDF extends UDF {
+
+    private final Set<Object> union = new HashSet<Object>();
+    private final Set<Object> intersect = new HashSet<Object>();
 
     public FloatWritable evaluate(long a, long b) {
         return evaluate(a, b, 128);
@@ -51,4 +59,26 @@ public final class JaccardIndexUDF extends UDF {
         float jaccard = countMatches / (float) k;
         return val(2.f * (jaccard - 0.5f));
     }
+
+    public FloatWritable evaluate(final List<String> a, final List<String> b) {
+        if(a == null || b == null) {
+            return new FloatWritable(0.f);
+        }
+        if(a.size() == 0 || b.size() == 0) {
+            return new FloatWritable(0.f);
+        }
+
+        union.addAll(a);
+        union.addAll(b);
+        float unionSize = union.size();
+        union.clear();
+
+        intersect.addAll(a);
+        intersect.retainAll(b);
+        float intersectSize = intersect.size();
+        intersect.clear();
+
+        return new FloatWritable(intersectSize / unionSize);
+    }
+
 }
