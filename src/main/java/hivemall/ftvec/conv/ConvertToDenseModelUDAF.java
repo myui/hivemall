645534e43629811d5a3979f784a817b91491c2b5
myui <yuin405@gmail.com>
2015-08-25 18:16:54 +0900

    Moved packages of to_dense/to_sparse/conv2dense to `hivemall.ftvec.conv`

--- src/main/java/hivemall/ftvec/conv/ConvertToDenseModelUDAF.java
+++ src/main/java/hivemall/ftvec/conv/ConvertToDenseModelUDAF.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package hivemall.ftvec;
+package hivemall.ftvec.conv;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/main/java/hivemall/ftvec/conv/ToSparseFeaturesUDF.java
+++ src/main/java/hivemall/ftvec/conv/ToSparseFeaturesUDF.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package hivemall.ftvec;
+package hivemall.ftvec.conv;
 
 import hivemall.utils.lang.StringUtils;
 
