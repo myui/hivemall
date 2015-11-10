318083bf4400c2239bde36578bfd5dd2cfb93604
myui <yuin405@gmail.com>
2015-11-10 16:57:22 +0900

    Changed the package of text processing to `hivemall.tools.text`

---
+++ core/src/main/java/hivemall/tools/text/StopwordUDF.java
@@ -0,0 +1,56 @@
+/*
+ * Hivemall: Hive scalable Machine Learning Library
+ *
+ * Copyright (C) 2015 Makoto YUI
+ * Copyright (C) 2013-2015 National Institute of Advanced Industrial Science and Technology (AIST)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hivemall.tools.text;
+
+import static hivemall.utils.hadoop.WritableUtils.val;
+
+import java.util.Arrays;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.udf.UDFType;
+import org.apache.hadoop.io.BooleanWritable;
+
+@Description(name = "is_stopword", value = "_FUNC_(word) - Returns whether stopword or not")
+@UDFType(deterministic = true, stateful = false)
+public final class StopwordUDF extends UDF {
+
+    private static final String[] stopwords;
+    static {
+        stopwords = new String[] { "i", "me", "my", "myself", "we", "our", "ours", "ourselves",
+                "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself",
+                "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
+                "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+                "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has",
+                "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but",
+                "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with",
+                "about", "against", "between", "into", "through", "during", "before", "after",
+                "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over",
+                "under", "again", "further", "then", "once", "here", "there", "when", "where",
+                "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some",
+                "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very",
+                "s", "t", "can", "will", "just", "don", "should", "now" };
+        Arrays.sort(stopwords);
+    }
+
+    public BooleanWritable evaluate(String word) {
+        return val(Arrays.binarySearch(stopwords, word) >= 0);
+    }
+
+}
