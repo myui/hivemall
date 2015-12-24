a4e0d0fad75901759cd63399b234ee7045c8f5ef
myui <yuin405@gmail.com>
2015-12-24 19:36:50 +0900

    Marked js_tree_predict and vm_tree_predicted as Deprecated

--- core/src/main/java/hivemall/smile/tools/TreePredictByStackMachineUDF.java
+++ core/src/main/java/hivemall/smile/tools/TreePredictByStackMachineUDF.java
@@ -42,6 +42,7 @@ import org.apache.hadoop.io.Writable;
 
 @Description(name = "vm_tree_predict", value = "_FUNC_(string script, array<double> features [, const boolean classification]) - Returns a prediction result of a random forest")
 @UDFType(deterministic = true, stateful = false)
+@Deprecated
 public final class TreePredictByStackMachineUDF extends GenericUDF {
 
     private boolean classification;
