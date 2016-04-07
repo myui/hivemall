3f60732450a516bfb7261b4bef079c52f5ca649d
Sotaro Sugimoto <sotaro.sugimoto@gmail.com>
2016-04-07 18:28:16 +0900

    Test now runs iterations over entire datafile bigdata.tr.txt is now read multiple times, and 1 iteration is considered to be 1 read through the entire file.

--- core/src/test/java/hivemall/fm/FieldAwareFactorizationMachineUDTFTest.java
+++ core/src/test/java/hivemall/fm/FieldAwareFactorizationMachineUDTFTest.java
@@ -34,59 +34,73 @@ import org.junit.Test;
 
 public class FieldAwareFactorizationMachineUDTFTest {
 
+    private final int TRAIN = 1000;
+    private final int MAX_LINES = 200;
+
     @Test
     public void test() throws HiveException, IOException {
         FieldAwareFactorizationMachineUDTF udtf = new FieldAwareFactorizationMachineUDTF();
         ObjectInspector[] argOIs = new ObjectInspector[] {
-                ObjectInspectorFactory.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaStringObjectInspector),
+                ObjectInspectorFactory.getStandardListObjectInspector(
+                    PrimitiveObjectInspectorFactory.javaStringObjectInspector),
                 PrimitiveObjectInspectorFactory.javaDoubleObjectInspector,
                 ObjectInspectorUtils.getConstantObjectInspector(
-                    PrimitiveObjectInspectorFactory.javaStringObjectInspector, "-factor 10")};
+                    PrimitiveObjectInspectorFactory.javaStringObjectInspector,
+                    "-classification -factor 10")};
 
         udtf.initialize(argOIs);
         FactorizationMachineModel model = udtf.getModel();
         Assert.assertTrue("Actual class: " + model.getClass().getName(),
             model instanceof FFMStringFeatureMapModel);
 
-        BufferedReader data = new BufferedReader(new InputStreamReader(
-            getClass().getResourceAsStream("bigdata.tr.txt")));
-        double loss = udtf._cvState.getCumulativeLoss();
-        for (int i = 0; i < 1000; ++i) {
-            //gather features in current line
-            final String input = data.readLine();
-            if (input == null) {
-                System.out.println("EOF reached");
-                break;
-            }
-            ArrayList<String> featureStrings = new ArrayList<String>();
-            ArrayList<StringFeature> features = new ArrayList<StringFeature>();
 
-            //make StringFeature for each word = data point
-            String remaining = input;
-            int wordCut = remaining.indexOf(' ');
-            while (wordCut != -1) {
-                featureStrings.add(remaining.substring(0, wordCut));
-                remaining = remaining.substring(wordCut + 1);
-                wordCut = remaining.indexOf(' ');
+        double cumul = 0.d;
+        for (int trainingIteration = 1; trainingIteration <= TRAIN; ++trainingIteration) {
+            BufferedReader data = new BufferedReader(
+                new InputStreamReader(getClass().getResourceAsStream("bigdata.tr.txt")));
+            double loss = udtf._cvState.getCumulativeLoss();
+            int lines = 0;
+            for (int lineNumber = 0; lineNumber < MAX_LINES; ++lineNumber, ++lines) {
+                //gather features in current line
+                final String input = data.readLine();
+                if (input == null) {
+                    System.out.println("EOF reached at line " + lineNumber);
+                    break;
+                }
+                ArrayList<String> featureStrings = new ArrayList<String>();
+                ArrayList<StringFeature> features = new ArrayList<StringFeature>();
+
+                //make StringFeature for each word = data point
+                String remaining = input;
+                int wordCut = remaining.indexOf(' ');
+                while (wordCut != -1) {
+                    featureStrings.add(remaining.substring(0, wordCut));
+                    remaining = remaining.substring(wordCut + 1);
+                    wordCut = remaining.indexOf(' ');
+                }
+                int end = featureStrings.size();
+                double y = Double.parseDouble(featureStrings.get(0));
+                if (y == 0) {
+                    y = -1;//LibFFM data uses {0, 1}; Hivemall uses {-1, 1}
+                }
+                for (int wordNumber = 1; wordNumber < end; ++wordNumber) {
+                    String entireFeature = featureStrings.get(wordNumber);
+                    int featureCut = StringUtils.ordinalIndexOf(entireFeature, ":", 2);
+                    String feature = entireFeature.substring(0, featureCut);
+                    double value = Double.parseDouble(entireFeature.substring(featureCut + 1));
+                    features.add(new StringFeature(feature, value));
+                }
+                udtf.process(new Object[] {toStringArray(features), y});
             }
-            int end = featureStrings.size();
-            double y = Double.parseDouble(featureStrings.get(0));
-            if (y == 0) {
-                y = -1;//LibFFM data uses {0, 1}; Hivemall uses {-1, 1}
-            }
-            for (int j = 1; j < end; ++j) {
-                String entireFeature = featureStrings.get(j);
-                int featureCut = StringUtils.ordinalIndexOf(entireFeature, ":", 2);
-                String feature = entireFeature.substring(0, featureCut);
-                double value = Double.parseDouble(entireFeature.substring(featureCut + 1));
-                features.add(new StringFeature(feature, value));
-            }
-            udtf.process(new Object[] {toStringArray(features), y});
-            loss = udtf._cvState.getCumulativeLoss() - loss;
-            System.out.println("loss for this iteration: " + loss
-                    + "; average loss up to this iteration: " + udtf._cvState.getCumulativeLoss()
-                    / i);
-            loss = udtf._cvState.getCumulativeLoss();
+            cumul = udtf._cvState.getCumulativeLoss();
+            loss = (cumul - loss) / lines;
+            // output with explanations
+            System.out.println(
+                "average loss for this iteration: " + loss + "; average loss up to this iteration: "
+                        + udtf._cvState.getCumulativeLoss() / (trainingIteration * lines));
+            // output for plotting
+//            System.out.println(loss + " " + cumul / (trainingIteration * lines));
+            data.close();
         }
     }
 
