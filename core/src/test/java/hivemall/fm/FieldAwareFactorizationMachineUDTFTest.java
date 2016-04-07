2d7858a75ba796d18012f9e360d08afbc684fa7a
myui <yuin405@gmail.com>
2016-04-07 15:08:41 +0900

    Changed tests() to throw Exceptions

--- core/src/test/java/hivemall/fm/FieldAwareFactorizationMachineUDTFTest.java
+++ core/src/test/java/hivemall/fm/FieldAwareFactorizationMachineUDTFTest.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
@@ -18,64 +17,54 @@ import org.junit.Test;
 public class FieldAwareFactorizationMachineUDTFTest {
 
     @Test
-    public void test() {
+    public void test() throws HiveException, IOException {
         FieldAwareFactorizationMachineUDTF udtf = new FieldAwareFactorizationMachineUDTF();
         ObjectInspector[] argOIs = new ObjectInspector[] {
             ObjectInspectorFactory.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaStringObjectInspector),
             PrimitiveObjectInspectorFactory.javaDoubleObjectInspector,
             ObjectInspectorUtils.getConstantObjectInspector(PrimitiveObjectInspectorFactory.javaStringObjectInspector, "-factor 10")
         };
-        try {
-            udtf.initialize(argOIs);
-        } catch (UDFArgumentException e1) {
-            e1.printStackTrace();
-        }
+        
+        udtf.initialize(argOIs);
         FactorizationMachineModel model = udtf.getModel();
         Assert.assertTrue("Actual class: " + model.getClass().getName(), model instanceof FFMStringFeatureMapModel);
         
-        BufferedReader data;
-        try {            
-            data = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("bigdata.tr.txt")));
-            double loss = udtf._cvState.getCumulativeLoss();
-            for(int i = 0; i < 1000; ++i) {
-                //gather features in current line
-                final String input = data.readLine();
-                if(input == null) {
-                    System.out.println("EOF reached");
-                    break;
-                }
-                ArrayList<String> featureStrings = new ArrayList<String>();
-                ArrayList<StringFeature> features = new ArrayList<StringFeature>();
-                
-                //make StringFeature for each word = data point
-                String remaining = input;
-                int wordCut = remaining.indexOf(' ');
-                while(wordCut != -1) {
-                    featureStrings.add(remaining.substring(0, wordCut));
-                    remaining = remaining.substring(wordCut + 1);
-                    wordCut = remaining.indexOf(' ');
-                }
-                int end = featureStrings.size();
-                double y = Double.parseDouble(featureStrings.get(0));
-                if(y == 0) {
-                    y = -1;//LibFFM data uses {0, 1}; Hivemall uses {-1, 1}
-                }
-                for (int j = 1; j < end; ++j) {
-                    String entireFeature = featureStrings.get(j);
-                    int featureCut = StringUtils.ordinalIndexOf(entireFeature, ":", 2);
-                    String feature = entireFeature.substring(0, featureCut);
-                    double value = Double.parseDouble(entireFeature.substring(featureCut + 1));
-                    features.add(new StringFeature(feature, value));
-                }
-                udtf.process(new Object[] { toStringArray(features), y });
-                loss = udtf._cvState.getCumulativeLoss() - loss;
-                System.out.println("loss for this iteration: " + loss + "; average loss up to this iteration: " + udtf._cvState.getCumulativeLoss()/i);
-                loss = udtf._cvState.getCumulativeLoss();
+        BufferedReader data = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("bigdata.tr.txt")));
+        double loss = udtf._cvState.getCumulativeLoss();
+        for(int i = 0; i < 1000; ++i) {
+            //gather features in current line
+            final String input = data.readLine();
+            if(input == null) {
+                System.out.println("EOF reached");
+                break;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (HiveException e) {
-            e.printStackTrace();
+            ArrayList<String> featureStrings = new ArrayList<String>();
+            ArrayList<StringFeature> features = new ArrayList<StringFeature>();
+            
+            //make StringFeature for each word = data point
+            String remaining = input;
+            int wordCut = remaining.indexOf(' ');
+            while(wordCut != -1) {
+                featureStrings.add(remaining.substring(0, wordCut));
+                remaining = remaining.substring(wordCut + 1);
+                wordCut = remaining.indexOf(' ');
+            }
+            int end = featureStrings.size();
+            double y = Double.parseDouble(featureStrings.get(0));
+            if(y == 0) {
+                y = -1;//LibFFM data uses {0, 1}; Hivemall uses {-1, 1}
+            }
+            for (int j = 1; j < end; ++j) {
+                String entireFeature = featureStrings.get(j);
+                int featureCut = StringUtils.ordinalIndexOf(entireFeature, ":", 2);
+                String feature = entireFeature.substring(0, featureCut);
+                double value = Double.parseDouble(entireFeature.substring(featureCut + 1));
+                features.add(new StringFeature(feature, value));
+            }
+            udtf.process(new Object[] { toStringArray(features), y });
+            loss = udtf._cvState.getCumulativeLoss() - loss;
+            System.out.println("loss for this iteration: " + loss + "; average loss up to this iteration: " + udtf._cvState.getCumulativeLoss()/i);
+            loss = udtf._cvState.getCumulativeLoss();
         }
     }
     
