5c178facc5b787845d0bb9c90ae1ec7b1a1cff18
myui <yuin405@gmail.com>
2016-04-11 19:42:32 +0900

    Changed BprSampling package

--- core/src/main/java/hivemall/ftvec/ranking/BprSamplingUDTF.java
+++ core/src/main/java/hivemall/ftvec/ranking/BprSamplingUDTF.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package hivemall.ftvec.sampling;
+package hivemall.ftvec.ranking;
 
 import hivemall.UDTFWithOptions;
 import hivemall.utils.hadoop.HiveUtils;
