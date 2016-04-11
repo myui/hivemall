5fdd91c00dedbdb8f84681f49ba72965a545cad4
myui <yuin405@gmail.com>
2016-04-11 17:49:50 +0900

    Fixed bpr_sampling UDTF to accept bitset input for pos_items

--- core/src/main/java/hivemall/ftvec/sampling/BprSamplingUDTF.java
+++ core/src/main/java/hivemall/ftvec/sampling/BprSamplingUDTF.java
@@ -18,7 +18,9 @@
  */
 package hivemall.ftvec.sampling;
 
+import hivemall.UDTFWithOptions;
 import hivemall.utils.hadoop.HiveUtils;
+import hivemall.utils.lang.Primitives;
 
 import java.util.ArrayList;
 import java.util.BitSet;
@@ -26,10 +28,11 @@ import java.util.Random;
 
 import javax.annotation.Nonnull;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
 import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
@@ -38,33 +41,72 @@ import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.io.IntWritable;
 
-@Description(
-        name = "bpr_sampling",
-        value = "_FUNC_(array<int> pos_items, const int max_item_id [, const double sampling_rates=1.0, const boolean withReplacement=true])"
+@Description(name = "bpr_sampling",
+        value = "_FUNC_(array<int|long> pos_items, const int max_item_id [, const string options])"
                 + "- Returns a relation consists of <int pos_item_id, int neg_item_id>")
-public final class BprSamplingUDTF extends GenericUDTF {
+public final class BprSamplingUDTF extends UDTFWithOptions {
 
     private ListObjectInspector listOI;
     private PrimitiveObjectInspector listElemOI;
+
     private int maxItemId;
+    private boolean bitsetInput;
     private float samplingRate;
-    private boolean withoutReplacement;
+    private boolean withReplacement;
 
     private Object[] forwardObjs;
     private IntWritable posItemId;
     private IntWritable negItemId;
 
-    private BitSet bitset;
-    private Random rand;
+    private BitSet _bitset;
+    private Random _rand;
 
     public BprSamplingUDTF() {}
 
     @Override
+    protected Options getOptions() {
+        Options opts = new Options();
+        opts.addOption("bitset", "bitset_input", true,
+            "Use Bitset for the input of pos_items [default:false]");
+        opts.addOption("sampling", "sampling_rate", true,
+            "Sampling rates of positive items [default: 1.0]");
+        opts.addOption("with_replacement", false, "Do sampling with-replacement [default: false]");
+        return opts;
+    }
+
+    @Override
+    protected CommandLine processOptions(ObjectInspector[] argOIs) throws UDFArgumentException {
+        CommandLine cl = null;
+
+        boolean bitsetInput = false;
+        float samplingRate = 1.f;
+        boolean withReplacement = false;
+        if (argOIs.length == 3) {
+            String args = HiveUtils.getConstString(argOIs[2]);
+            cl = parseOptions(args);
+
+            bitsetInput = cl.hasOption("bitset_input");
+            withReplacement = cl.hasOption("with_replacement");
+            samplingRate = Primitives.parseFloat(cl.getOptionValue("sampling_rate"), samplingRate);
+            if (withReplacement == false && samplingRate > 1.f) {
+                throw new UDFArgumentException(
+                    "sampling_rate MUST be in less than or equals to 1 where withReplacement is false: "
+                            + samplingRate);
+            }
+        }
+
+        this.bitsetInput = bitsetInput;
+        this.samplingRate = samplingRate;
+        this.withReplacement = withReplacement;
+        return cl;
+    }
+
+    @Override
     public StructObjectInspector initialize(ObjectInspector[] argOIs) throws UDFArgumentException {
-        if (argOIs.length < 2 || argOIs.length > 4) {
-            throw new UDFArgumentException("bpr_sampling(array<long>, const long max_item_id "
-                    + "[, const double sampling_rate, const boolean withoutReplacement=false])"
-                    + " takes at least two arguments");
+        if (argOIs.length != 2 && argOIs.length != 3) {
+            throw new UDFArgumentException(
+                "bpr_sampling(array<long>, const long max_item_id [, const string options])"
+                        + " takes at least two arguments");
         }
         this.listOI = HiveUtils.asListOI(argOIs[0]);
         this.listElemOI = HiveUtils.asPrimitiveObjectInspector(listOI.getListElementObjectInspector());
@@ -73,26 +115,6 @@ public final class BprSamplingUDTF extends GenericUDTF {
         if (maxItemId <= 0) {
             throw new UDFArgumentException("maxItemId MUST be greater than 0: " + maxItemId);
         }
-
-        if (argOIs.length == 4) {
-            this.withoutReplacement = HiveUtils.getConstBoolean(argOIs[3]);
-        } else {
-            this.withoutReplacement = false;
-        }
-
-        float rate = 1.f;
-        if (argOIs.length >= 3) {
-            rate = HiveUtils.getAsConstFloat(argOIs[2]);
-            if (rate <= 0.f) {
-                throw new UDFArgumentException("sampling_rate MUST be greater than 0: " + rate);
-            }
-            if (withoutReplacement && rate > 1.f) {
-                throw new UDFArgumentException(
-                    "sampling_rate MUST be in less than or equals to 1 where withoutReplacement is true: "
-                            + rate);
-            }
-        }
-        this.samplingRate = rate;
 
         this.posItemId = new IntWritable();
         this.negItemId = new IntWritable();
@@ -110,14 +132,27 @@ public final class BprSamplingUDTF extends GenericUDTF {
 
     @Override
     public void process(Object[] args) throws HiveException {
-        if (bitset == null) {
-            this.bitset = new BitSet();
-            this.rand = new Random(43);
+        final int numPosItems;
+        final BitSet bs;
+        if (bitsetInput) {
+            if (_rand == null) {
+                this._rand = new Random(43);
+            }
+            long[] longs = HiveUtils.asLongArray(args[0], listOI, listElemOI);
+            bs = BitSet.valueOf(longs);
+            numPosItems = bs.cardinality();
         } else {
-            bitset.clear();
+            if (_bitset == null) {
+                bs = new BitSet();
+                this._bitset = bs;
+                this._rand = new Random(43);
+            } else {
+                bs = _bitset;
+                bs.clear();
+            }
+            numPosItems = HiveUtils.setBits(args[0], listOI, listElemOI, bs);
         }
 
-        final int numPosItems = HiveUtils.setBits(args[0], listOI, listElemOI, bitset);
         if (numPosItems == 0) {
             return;
         }
@@ -129,10 +164,43 @@ public final class BprSamplingUDTF extends GenericUDTF {
                     + numPosItems + " = " + numNegItems);
         }
 
-        if (withoutReplacement) {
-            sampleWithoutReplacement(numPosItems, numNegItems, bitset);
+        if (withReplacement) {
+            sampleWithReplacement(numPosItems, numNegItems, bs);
         } else {
-            sampleWithReplacement(numPosItems, numNegItems, bitset);
+            sampleWithoutReplacement(numPosItems, numNegItems, bs);
+        }
+    }
+
+    private void sampleWithReplacement(final int numPosItems, final int numNegItems,
+            @Nonnull final BitSet bitset) throws HiveException {
+        final int numSamples = Math.max(1, Math.round(numPosItems * samplingRate));
+        for (int s = 0; s < numSamples; s++) {
+            int nth = _rand.nextInt(numPosItems);
+
+            int i = bitset.nextSetBit(0);
+            for (int c = 0; i >= 0; i = bitset.nextSetBit(i + 1), c++) {
+                if (c == nth) {
+                    break;
+                }
+            }
+            if (i == -1) {
+                throw new UDFArgumentException("Illegal i value: " + i);
+            }
+
+            nth = _rand.nextInt(numNegItems);
+            int j = bitset.nextClearBit(0);
+            for (int c = 0; j <= maxItemId; j = bitset.nextClearBit(j + 1), c++) {
+                if (c == nth) {
+                    break;
+                }
+            }
+            if (j < 0 || j > maxItemId) {
+                throw new UDFArgumentException("j MUST be in [0," + maxItemId + "] but j was " + j);
+            }
+
+            posItemId.set(i);
+            negItemId.set(j);
+            forward(forwardObjs);
         }
     }
 
@@ -144,7 +212,7 @@ public final class BprSamplingUDTF extends GenericUDTF {
 
         final int numSamples = Math.max(1, Math.round(numPosItems * samplingRate));
         for (int s = 0; s < numSamples; s++) {
-            int nth = rand.nextInt(numPosItems);
+            int nth = _rand.nextInt(numPosItems);
 
             int i = bitsetForPosSampling.nextSetBit(0);
             for (int c = 0; i >= 0; i = bitsetForPosSampling.nextSetBit(i + 1), c++) {
@@ -158,7 +226,7 @@ public final class BprSamplingUDTF extends GenericUDTF {
             bitsetForPosSampling.set(i, false);
             --numPosItems;
 
-            nth = rand.nextInt(numNegItems);
+            nth = _rand.nextInt(numNegItems);
             int j = bitsetForNegSampling.nextClearBit(0);
             for (int c = 0; j <= maxItemId; j = bitsetForNegSampling.nextClearBit(j + 1), c++) {
                 if (c == nth) {
@@ -185,39 +253,6 @@ public final class BprSamplingUDTF extends GenericUDTF {
         }
     }
 
-    private void sampleWithReplacement(final int numPosItems, final int numNegItems,
-            @Nonnull final BitSet bitset) throws HiveException {
-        final int numSamples = Math.max(1, Math.round(numPosItems * samplingRate));
-        for (int s = 0; s < numSamples; s++) {
-            int nth = rand.nextInt(numPosItems);
-
-            int i = bitset.nextSetBit(0);
-            for (int c = 0; i >= 0; i = bitset.nextSetBit(i + 1), c++) {
-                if (c == nth) {
-                    break;
-                }
-            }
-            if (i == -1) {
-                throw new UDFArgumentException("Illegal i value: " + i);
-            }
-
-            nth = rand.nextInt(numNegItems);
-            int j = bitset.nextClearBit(0);
-            for (int c = 0; j <= maxItemId; j = bitset.nextClearBit(j + 1), c++) {
-                if (c == nth) {
-                    break;
-                }
-            }
-            if (j < 0 || j > maxItemId) {
-                throw new UDFArgumentException("j MUST be in [0," + maxItemId + "] but j was " + j);
-            }
-
-            posItemId.set(i);
-            negItemId.set(j);
-            forward(forwardObjs);
-        }
-    }
-
     @Override
     public void close() throws HiveException {
         this.listOI = null;
@@ -225,8 +260,8 @@ public final class BprSamplingUDTF extends GenericUDTF {
         this.forwardObjs = null;
         this.posItemId = null;
         this.negItemId = null;
-        this.bitset = null;
-        this.rand = null;
+        this._bitset = null;
+        this._rand = null;
     }
 
 }
