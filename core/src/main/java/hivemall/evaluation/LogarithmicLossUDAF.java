c6c1db1ac67e8fca31fa57a26128c336cb4d2c62
Takayuki Sakai <kaky0922@gmail.com>
2016-05-19 10:17:00 +0900

    Add Logarithmic Loss UDAF to evaluation functions

---
+++ core/src/main/java/hivemall/evaluation/LogarithmicLossUDAF.java
@@ -0,0 +1,103 @@
+/*
+ * Hivemall: Hive scalable Machine Learning Library
+ *
+ * Copyright (C) 2015 Makoto YUI
+ * Copyright (C) 2013-2015 National Institute of Advanced Industrial Science and Technology (AIST)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hivemall.evaluation;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDAF;
+import org.apache.hadoop.hive.ql.exec.UDAFEvaluator;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
+
+@Description(name = "logloss", value = "_FUNC_(predicted, actual) - Return a Logrithmic Loss")
+public final class LogarithmicLossUDAF extends UDAF {
+
+    public static class Evaluator implements UDAFEvaluator {
+
+        private PartialResult partial;
+
+        public Evaluator() {}
+
+        @Override
+        public void init() {
+            this.partial = null;
+        }
+
+        public boolean iterate(DoubleWritable predicted, DoubleWritable actual)
+                throws HiveException {
+            if(predicted == null || actual == null) {// skip
+                return true;
+            }
+            if(partial == null) {
+                this.partial = new PartialResult();
+            }
+            partial.iterate(predicted.get(), actual.get());
+            return true;
+        }
+
+        public PartialResult terminatePartial() {
+            return partial;
+        }
+
+        public boolean merge(PartialResult other) throws HiveException {
+            if(other == null) {
+                return true;
+            }
+            if(partial == null) {
+                this.partial = new PartialResult();
+            }
+            partial.merge(other);
+            return true;
+        }
+
+        public double terminate() {
+            if(partial == null) {
+                return 0.d;
+            }
+            return partial.getLogLoss();
+        }
+    }
+
+    public static class PartialResult {
+
+        double log_sum;
+        long count;
+
+        PartialResult() {
+            this.log_sum = 0d;
+            this.count = 0L;
+        }
+
+        void iterate(double predicted, double actual) {
+            double epsilon = 1e-15;
+            predicted = Math.max(epsilon, predicted);
+            predicted = Math.min(1-epsilon, predicted);
+            log_sum += actual * Math.log(predicted) + (1-actual) * Math.log(1-predicted);
+            count++;
+        }
+
+        void merge(PartialResult other) {
+            log_sum += other.log_sum;
+            count += other.count;
+        }
+
+        double getLogLoss() { return -1 * log_sum / count; }
+
+    }
+
+}
