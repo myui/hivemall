076d36a576dac6e6806206549e1a548fa3aa2406
myui <yuin405@gmail.com>
2016-06-03 16:18:28 +0900

    Added HeapBuffer class

---
+++ core/src/main/java/hivemall/utils/buffer/HeapBuffer.java
@@ -0,0 +1,323 @@
+/*
+ * Hivemall: Hive scalable Machine Learning Library
+ *
+ * Copyright (C) 2015 Makoto YUI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hivemall.utils.buffer;
+
+import hivemall.utils.lang.NumberUtils;
+import hivemall.utils.lang.Preconditions;
+import hivemall.utils.lang.Primitives;
+import hivemall.utils.lang.SizeOf;
+import hivemall.utils.lang.UnsafeUtils;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.NotThreadSafe;
+
+import sun.misc.Unsafe;
+
+@SuppressWarnings("restriction")
+@NotThreadSafe
+public final class HeapBuffer {
+    /** 4 (int) * 4 MiB = 16 MiB */
+    public static final int DEFAULT_CHUNK_SIZE = 4 * 1024 * 1024;
+    /** 16 MiB * 8 Chunks = 128 MiB */
+    public static final int DEFAULT_NUM_CHUNKS = 8;
+
+    @Nonnull
+    private final Unsafe _UNSAFE;
+
+    private final int _chunkSize;
+    @Nonnull
+    private int[][] _chunks;
+    /** the number of chunks created */
+    private int _initializedChunks;
+
+    /** physical address pointer */
+    private long _position;
+
+    /** Total allocated bytes */
+    private long _allocatedBytes;
+
+    public HeapBuffer() {
+        this(DEFAULT_NUM_CHUNKS, DEFAULT_CHUNK_SIZE);
+    }
+
+    public HeapBuffer(int initNumChunks, int chunkSize) {
+        this._UNSAFE = UnsafeUtils.getUnsafe();
+        this._chunkSize = chunkSize;
+        this._chunks = new int[initNumChunks][];
+        this._initializedChunks = 0;
+        this._position = 0L;
+        this._allocatedBytes = 0L;
+    }
+
+    /**
+     * @param bytes allocating bytes
+     * @return pointer of the allocated object
+     */
+    public long allocate(final int bytes) {
+        Preconditions.checkArgument(bytes > 0, "Failed to allocate bytes : %s", bytes);
+        Preconditions.checkArgument(bytes <= _chunkSize,
+            "Cannot allocate memory greater than %s bytes: %s", _chunkSize, bytes);
+
+        int i = Primitives.castToInt(_position / _chunkSize);
+        final int j = Primitives.castToInt(_position % _chunkSize);
+        if (bytes > (_chunkSize - j)) {
+            // cannot allocate the object in the current chunk
+            // so, skip the current chunk
+            i++;
+            _position = ((long) i) * _chunkSize;
+        }
+        grow(i);
+
+        long ptr = _position;
+        this._position += bytes;
+        this._allocatedBytes += bytes;
+
+        return ptr;
+    }
+
+    private void grow(final int chunkIndex) {
+        if (chunkIndex < _initializedChunks) {
+            return; // no need to grow
+        }
+
+        int[][] chunks = _chunks;
+        if (chunkIndex >= _chunks.length) {
+            int newSize = Math.max(chunkIndex + 1, _chunks.length * 2);
+            int[][] newChunks = new int[newSize][];
+            System.arraycopy(_chunks, 0, newChunks, 0, _chunks.length);
+            this._chunks = newChunks;
+            chunks = newChunks;
+        }
+        for (int i = _initializedChunks; i <= chunkIndex; ++i) {
+            chunks[i] = new int[_chunkSize];
+        }
+        this._initializedChunks = chunkIndex + 1;
+    }
+
+    private void validatePointer(final long ptr) {
+        if (ptr >= _position) {
+            throw new IllegalArgumentException("Invalid pointer " + ptr + " does not in range [0,"
+                    + _position + ')');
+        }
+    }
+
+    public byte getByte(final long ptr) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getByte(chunk, j);
+    }
+
+    public void putByte(final long ptr, final byte value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putByte(chunk, j, value);
+    }
+
+    public int getInt(final long ptr) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getInt(chunk, j);
+    }
+
+    public void putInt(final long ptr, final int value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putInt(chunk, j, value);
+    }
+
+    public short getShort(final long ptr) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getShort(chunk, j);
+    }
+
+    public void putShort(final long ptr, final short value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putShort(chunk, j, value);
+    }
+
+    public char getChar(final long ptr) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getChar(chunk, j);
+    }
+
+    public void putChar(final long ptr, final char value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putChar(chunk, j, value);
+    }
+
+    public long getLong(final long ptr) {
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getLong(chunk, j);
+    }
+
+    public void putLong(final long ptr, final long value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putLong(chunk, j, value);
+    }
+
+    public float getFloat(final long ptr) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getFloat(chunk, j);
+    }
+
+    public void putFloat(final long ptr, final float value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putFloat(chunk, j, value);
+    }
+
+    public double getDouble(final long ptr) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        return _UNSAFE.getDouble(chunk, j);
+    }
+
+    public void putDouble(final long ptr, final double value) {
+        validatePointer(ptr);
+        int i = Primitives.castToInt(ptr / _chunkSize);
+        int[] chunk = _chunks[i];
+        long j = offset(ptr);
+        _UNSAFE.putDouble(chunk, j, value);
+    }
+
+    public void getFloats(final long ptr, @Nonnull final float[] values) {
+        validatePointer(ptr);
+        final int len = values.length;
+        if (len == 0) {
+            throw new IllegalArgumentException("Cannot put empty array at " + ptr);
+        }
+
+        int chunkIdx = Primitives.castToInt(ptr / _chunkSize);
+        final int[] chunk = _chunks[chunkIdx];
+        final long base = offset(ptr);
+        for (int i = 0; i < len; i++) {
+            long offset = base + SizeOf.FLOAT * i;
+            validateOffset(offset);
+            values[i] = _UNSAFE.getFloat(chunk, offset);
+        }
+    }
+
+    public void putFloats(final long ptr, @Nonnull final float[] values) {
+        validatePointer(ptr);
+        final int len = values.length;
+        if (len == 0) {
+            throw new IllegalArgumentException("Cannot put empty array at " + ptr);
+        }
+
+        int chunkIdx = Primitives.castToInt(ptr / _chunkSize);
+        final int[] chunk = _chunks[chunkIdx];
+        final long base = offset(ptr);
+        for (int i = 0; i < len; i++) {
+            long offset = base + SizeOf.FLOAT * i;
+            validateOffset(offset);
+            _UNSAFE.putFloat(chunk, offset, values[i]);
+        }
+    }
+
+    private void validateOffset(final long offset) {
+        if (offset >= _chunkSize) {
+            throw new IndexOutOfBoundsException("Invalid offset " + offset + " not in range [0,"
+                    + _chunkSize + ')');
+        }
+    }
+
+    /**
+     * Returns an offset in a chunk
+     * 
+     * @param ptr physical address
+     * @return physical offset in a chunk
+     */
+    private long offset(final long ptr) {
+        long j = ptr % _chunkSize;
+        return Unsafe.ARRAY_INT_BASE_OFFSET + j;
+    }
+
+    @Override
+    public String toString() {
+        return "HeapBuffer [position=" + NumberUtils.formatNumber(_position) + ", #consumed="
+                + NumberUtils.prettySize(consumedBytes()) + ", #allocated="
+                + NumberUtils.prettySize(_allocatedBytes) + ", #chunks="
+                + NumberUtils.formatNumber(_chunks.length) + ", #initializedChunks="
+                + NumberUtils.formatNumber(_initializedChunks) + ", #chunkSize="
+                + NumberUtils.prettySize(_chunkSize) + "]";
+    }
+
+    long position() {
+        return _position;
+    }
+
+    long consumedBytes() {
+        long bytes = 0L;
+        for (int[] c : _chunks) {
+            if (c == null) {
+                continue;
+            }
+            bytes += SizeOf.INT * c.length;
+        }
+        return bytes;
+    }
+
+    long allocatedBytes() {
+        return _allocatedBytes;
+    }
+
+    int getNumChunks() {
+        return _chunks.length;
+    }
+
+    int getNumInitializedChunks() {
+        return _initializedChunks;
+    }
+
+    int getChunkSize() {
+        return _chunkSize;
+    }
+
+}
---
+++ core/src/test/java/hivemall/utils/buffer/HeapBufferTest.java
@@ -0,0 +1,216 @@
+/*
+ * Hivemall: Hive scalable Machine Learning Library
+ *
+ * Copyright (C) 2015 Makoto YUI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hivemall.utils.buffer;
+
+import hivemall.utils.lang.Preconditions;
+import hivemall.utils.lang.SizeOf;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HeapBufferTest {
+
+    @Test
+    public void testSingleEntry() {
+        final int factors = 5;
+        HeapBuffer heap = new HeapBuffer();
+
+        Entry1 e1 = new Entry1(heap, factors);
+        long ptr1 = heap.allocate(e1.size());
+        e1.setOffset(ptr1);
+
+        float w1 = 1.f;
+        float[] v1 = new float[factors];
+        for (int i = 0; i < factors; i++) {
+            v1[i] = i;
+        }
+
+        e1.setW(w1);
+        e1.setV(v1);
+
+        float[] vProbe = new float[factors];
+        Assert.assertEquals(w1, e1.getW(), 0.f);
+        e1.getV(vProbe);
+        Assert.assertArrayEquals(v1, vProbe, 0.f);
+    }
+
+    @Test
+    public void testMultipleEntries() {
+        final int factors = 5;
+        final HeapBuffer buf = new HeapBuffer();
+
+        Entry1 e1 = new Entry1(buf, factors);
+        long ptr1 = buf.allocate(e1.size());
+        e1.setOffset(ptr1);
+
+        float w1 = 1.f;
+        float[] v1 = new float[factors];
+        for (int i = 0; i < factors; i++) {
+            v1[i] = 1 + i;
+        }
+
+        e1.setW(w1);
+        e1.setV(v1);
+
+        float[] vProbe = new float[factors];
+        Assert.assertEquals(w1, e1.getW(), 0.f);
+        e1.getV(vProbe);
+        Assert.assertArrayEquals(v1, vProbe, 0.f);
+
+        w1 = 1.1f;
+        e1.setW(w1);
+        Assert.assertEquals(w1, e1.getW(), 0.f);
+
+        Assert.assertEquals(e1.size(), buf.allocatedBytes());
+
+        Entry2 e2 = new Entry2(buf, factors);
+        long ptr2 = buf.allocate(e2.size());
+        e2.setOffset(ptr2);
+
+        float w2 = 2.f;
+        float[] v2 = new float[factors];
+        for (int i = 0; i < factors; i++) {
+            v2[i] = 2 + i;
+        }
+        double z2 = 2.d;
+
+        e2.setW(w2);
+        e2.setV(v2);
+        e2.setZ(z2);
+
+        Assert.assertEquals(w2, e2.getW(), 0.f);
+        e2.getV(vProbe);
+        Assert.assertArrayEquals(v2, vProbe, 0.f);
+        Assert.assertEquals(z2, e2.getZ(), 0.d);
+        for (int i = 0; i < factors; i++) {
+            v2[i] = 2.1f + i;
+        }
+        e2.setV(v2);
+        Assert.assertEquals(z2, e2.getZ(), 0.d);
+
+        Assert.assertEquals(w1, e1.getW(), 0.f);
+        e1.getV(vProbe);
+        Assert.assertArrayEquals(v1, vProbe, 0.f);
+
+        Assert.assertEquals(e1.size() + e2.size(), buf.allocatedBytes());
+    }
+
+    @Test
+    public void testLargeEntries() {
+        final int loop = 10000;
+        final int factors = 5;
+        final float[] v = new float[factors];
+        final HeapBuffer buf = new HeapBuffer(2, 1024);
+        final long[] ptrs = new long[loop];
+
+        final Entry1 entryProbe = new Entry1(buf, factors);
+        for (int i = 0; i < loop; i++) {
+            entryProbe.setOffset(buf.allocate(entryProbe.size()));
+            ptrs[i] = entryProbe.getOffset();
+            entryProbe.setW(i);
+            for (int f = 0; f < factors; f++) {
+                v[f] = i + f;
+            }
+            entryProbe.setV(v);
+        }
+
+        for (int i = 0; i < loop; i++) {
+            entryProbe.setOffset(ptrs[i]);
+            Assert.assertEquals(i, entryProbe.getW(), 0.f);
+            entryProbe.getV(v);
+            for (int f = 0; f < factors; f++) {
+                Assert.assertEquals(i + f, v[f], 0.f);
+            }
+        }
+    }
+
+    private static class Entry1 {
+
+        @Nonnull
+        final HeapBuffer buf;
+        final int size;
+        final int factors;
+        long offset;
+
+        Entry1(@Nonnull HeapBuffer buf, int size, int factors) {
+            this.buf = buf;
+            this.size = size;
+            this.factors = factors;
+        }
+
+        Entry1(@Nonnull HeapBuffer buf, int factors) {
+            this(buf, getSize(factors), factors);
+        }
+
+        int size() {
+            return size;
+        }
+
+        void setOffset(long offset) {
+            this.offset = offset;
+        }
+
+        long getOffset() {
+            return offset;
+        }
+
+        void setW(float w) {
+            buf.putFloat(offset, w);
+        }
+
+        float getW() {
+            return buf.getFloat(offset);
+        }
+
+        void setV(@Nonnull float[] v) {
+            Preconditions.checkArgument(v.length == factors);
+            buf.putFloats(offset + SizeOf.FLOAT, v);
+        }
+
+        void getV(@Nonnull float[] v) {
+            Preconditions.checkArgument(v.length == factors);
+            buf.getFloats(offset + SizeOf.FLOAT, v);
+        }
+
+        static int getSize(int factors) {
+            return SizeOf.FLOAT + SizeOf.FLOAT * factors;
+        }
+    }
+
+    private static class Entry2 extends Entry1 {
+
+        Entry2(@Nonnull HeapBuffer buf, int factors) {
+            super(buf, getSize(factors), factors);
+        }
+
+        void setZ(double z) {
+            buf.putDouble(offset + SizeOf.FLOAT + SizeOf.FLOAT * factors, z);
+        }
+
+        double getZ() {
+            return buf.getDouble(offset + SizeOf.FLOAT + SizeOf.FLOAT * factors);
+        }
+
+        static int getSize(int factors) {
+            return Entry1.getSize(factors) + SizeOf.DOUBLE;
+        }
+    }
+
+}
