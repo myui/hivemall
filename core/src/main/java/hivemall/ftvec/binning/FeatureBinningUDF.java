fb52406727e26661aaf8369496c725da18410726
amaya <amaya382@users.noreply.github.com>
2016-10-07 20:17:40 +0900

    Hotfix/feature binning (#365)
    * add map operators
    
    * refine feature binning

--- core/src/main/java/hivemall/ftvec/binning/FeatureBinningUDF.java
+++ core/src/main/java/hivemall/ftvec/binning/FeatureBinningUDF.java
@@ -18,12 +18,6 @@
 package hivemall.ftvec.binning;
 
 import hivemall.utils.hadoop.HiveUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
@@ -31,34 +25,34 @@ import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.UDFType;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector.Category;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.StandardListObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.StandardMapObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableDoubleObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableStringObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
+import java.util.*;
+
 @Description(
         name = "feature_binning",
-        value = "_FUNC_(array<features::string> features, const map<string, array<double>> quantiles_map)"
-                + " / _FUNC(int|bigint|float|double weight, const array<double> quantiles)"
+        value = "_FUNC_(array<features::string> features, const map<string, array<number>> quantiles_map)"
+                + " / _FUNC(number weight, const array<number> quantiles)"
                 + " - Returns binned features as an array<features::string> / bin ID as int")
 @UDFType(deterministic = true, stateful = false)
 public final class FeatureBinningUDF extends GenericUDF {
     private boolean multiple = true;
 
-    private StandardListObjectInspector featuresOI;
-    private WritableStringObjectInspector featuresElOI;
-    private StandardMapObjectInspector quantilesMapOI;
-    private WritableStringObjectInspector keyOI;
-    private StandardListObjectInspector quantilesOI;
-    private WritableDoubleObjectInspector quantileOI;
+    private ListObjectInspector featuresOI;
+    private StringObjectInspector featureOI;
+    private MapObjectInspector quantilesMapOI;
+    private StringObjectInspector keyOI;
+    private ListObjectInspector quantilesOI;
+    private PrimitiveObjectInspector quantileOI;
 
     private PrimitiveObjectInspector weightOI;
 
@@ -67,74 +61,58 @@ public final class FeatureBinningUDF extends GenericUDF {
 
     @Override
     public ObjectInspector initialize(ObjectInspector[] OIs) throws UDFArgumentException {
-        if (OIs.length != 2)
+        if (OIs.length != 2) {
             throw new UDFArgumentLengthException("Specify two arguments");
+        }
 
-        Category arg0Category = OIs[0].getCategory();
-        Category arg1Category = OIs[1].getCategory();
+        if (HiveUtils.isListOI(OIs[0]) && HiveUtils.isMapOI(OIs[1])) {
+            // for (array<features::string> features, const map<string, array<number>> quantiles_map)
 
-        if (arg0Category == Category.LIST && arg1Category == Category.MAP) {
-            featuresOI = (StandardListObjectInspector) OIs[0];
-            isStringOI(featuresOI.getListElementObjectInspector(), 0, "features");
-            featuresElOI = (WritableStringObjectInspector) featuresOI.getListElementObjectInspector();
-            quantilesMapOI = (StandardMapObjectInspector) OIs[1];
-            isStringOI(quantilesMapOI.getMapKeyObjectInspector(), 1, "key of quantiles_map");
-            keyOI = (WritableStringObjectInspector) quantilesMapOI.getMapKeyObjectInspector();
-            isListOI(quantilesMapOI.getMapValueObjectInspector(), 1, "value of quantiles_map");
-            quantilesOI = (StandardListObjectInspector) quantilesMapOI.getMapValueObjectInspector();
-            isNumberOI(quantilesOI.getListElementObjectInspector(), 1, "value of quantiles");
-            quantileOI = (WritableDoubleObjectInspector) quantilesOI.getListElementObjectInspector();
+            if (!HiveUtils.isStringOI(((ListObjectInspector) OIs[0]).getListElementObjectInspector())) {
+                throw new UDFArgumentTypeException(0,
+                    "Only array<string> type argument is acceptable but " + OIs[0].getTypeName()
+                            + " was passed as `features`");
+            }
+            featuresOI = HiveUtils.asListOI(OIs[0]);
+            featureOI = HiveUtils.asStringOI(featuresOI.getListElementObjectInspector());
+
+            quantilesMapOI = HiveUtils.asMapOI(OIs[1]);
+            if (!HiveUtils.isStringOI(quantilesMapOI.getMapKeyObjectInspector())
+                    || !HiveUtils.isListOI(quantilesMapOI.getMapValueObjectInspector())
+                    || !HiveUtils.isNumberOI(((ListObjectInspector) quantilesMapOI.getMapValueObjectInspector()).getListElementObjectInspector())) {
+                throw new UDFArgumentTypeException(1,
+                    "Only map<string, array<number>> type argument is acceptable but "
+                            + OIs[1].getTypeName() + " was passed as `quantiles_map`");
+            }
+            keyOI = HiveUtils.asStringOI(quantilesMapOI.getMapKeyObjectInspector());
+            quantilesOI = HiveUtils.asListOI(quantilesMapOI.getMapValueObjectInspector());
+            quantileOI = HiveUtils.asDoubleCompatibleOI(quantilesOI.getListElementObjectInspector());
 
             multiple = true;
 
             return ObjectInspectorFactory.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.writableStringObjectInspector);
-        } else if (arg0Category == Category.PRIMITIVE && arg1Category == Category.LIST) {
-            isNumberOI(OIs[0], 0, "weight");
-            weightOI = (PrimitiveObjectInspector) OIs[0];
-            quantilesOI = (StandardListObjectInspector) OIs[1];
-            isNumberOI(quantilesOI.getListElementObjectInspector(), 1, "value of quantiles");
-            quantileOI = (WritableDoubleObjectInspector) quantilesOI.getListElementObjectInspector();
+        } else if (HiveUtils.isPrimitiveOI(OIs[0]) && HiveUtils.isListOI(OIs[1])) {
+            // for (number weight, const array<number> quantiles)
+
+            weightOI = HiveUtils.asDoubleCompatibleOI(OIs[0]);
+
+            quantilesOI = HiveUtils.asListOI(OIs[1]);
+            if (!HiveUtils.isNumberOI(quantilesOI.getListElementObjectInspector())) {
+                throw new UDFArgumentTypeException(1,
+                    "Only array<number> type argument is acceptable but " + OIs[1].getTypeName()
+                            + " was passed as `quantiles`");
+            }
+            quantileOI = HiveUtils.asDoubleCompatibleOI(quantilesOI.getListElementObjectInspector());
 
             multiple = false;
 
             return PrimitiveObjectInspectorFactory.writableIntObjectInspector;
         } else {
-            throw new UDFArgumentTypeException(
-                0,
-                "Only <array<features::string>, map<string, array<double>>> "
-                        + "or <int|bigint|float|double, array<double>> type arguments are accepted but <"
+            throw new UDFArgumentTypeException(0,
+                "Only <array<features::string>, map<string, array<number>>> "
+                        + "or <number, array<number>> type arguments are accepted but <"
                         + OIs[0].getTypeName() + ", " + OIs[1].getTypeName() + "> was passed.");
         }
-    }
-
-    private boolean isStringOI(ObjectInspector OI, int index, String name)
-            throws UDFArgumentTypeException {
-        if (HiveUtils.isStringOI(OI))
-            return true;
-        else
-            throw new UDFArgumentTypeException(index,
-                "Only string type arguments are accepted but " + OI.getTypeName()
-                        + " was passed as `" + name + "`.");
-    }
-
-    private boolean isListOI(ObjectInspector OI, int index, String name)
-            throws UDFArgumentTypeException {
-        if (HiveUtils.isListOI(OI))
-            return true;
-        else
-            throw new UDFArgumentTypeException(index, "Only array type arguments are accepted but "
-                    + OI.getTypeName() + " was passed as `" + name + "`.");
-
-    }
-
-    private boolean isNumberOI(ObjectInspector OI, int index, String name)
-            throws UDFArgumentTypeException {
-        if (HiveUtils.isNumberOI(OI))
-            return true;
-        else
-            throw new UDFArgumentTypeException(index,
-                "Only numeric type arguments are accepted but " + OI.getTypeName()
-                        + " was passed as `" + name + "`.");
     }
 
     @Override
@@ -143,28 +121,28 @@ public final class FeatureBinningUDF extends GenericUDF {
             // init quantilesMap
             if (quantilesMap == null) {
                 quantilesMap = new HashMap<Text, double[]>();
-                Map<?, ?> _quantilesMap = quantilesMapOI.getMap(dObj[1].get());
+                final Map<?, ?> _quantilesMap = quantilesMapOI.getMap(dObj[1].get());
 
                 for (Object _key : _quantilesMap.keySet()) {
-                    Text key = new Text(keyOI.getPrimitiveJavaObject(_key));
-                    double[] val = HiveUtils.asDoubleArray(_quantilesMap.get(key), quantilesOI,
-                        quantileOI);
+                    final Text key = new Text(keyOI.getPrimitiveJavaObject(_key));
+                    final double[] val = HiveUtils.asDoubleArray(_quantilesMap.get(key),
+                        quantilesOI, quantileOI);
                     quantilesMap.put(key, val);
                 }
             }
 
-            List<?> fs = featuresOI.getList(dObj[0].get());
-            List<Text> result = new ArrayList<Text>();
+            final List<?> fs = featuresOI.getList(dObj[0].get());
+            final List<Text> result = new ArrayList<Text>();
             for (Object f : fs) {
-                String entry = featuresElOI.getPrimitiveJavaObject(f);
-                int pos = entry.indexOf(":");
+                final String entry = featureOI.getPrimitiveJavaObject(f);
+                final int pos = entry.indexOf(":");
 
                 if (pos < 0) {
                     // categorical
                     result.add(new Text(entry));
                 } else {
                     // quantitative
-                    Text key = new Text(entry.substring(0, pos));
+                    final Text key = new Text(entry.substring(0, pos));
                     String val = entry.substring(pos + 1);
 
                     // binning
@@ -187,30 +165,20 @@ public final class FeatureBinningUDF extends GenericUDF {
         }
     }
 
-    private int findBin(double[] _quantiles, double target) throws HiveException {
-        if (_quantiles.length < 3)
+    private int findBin(double[] _quantiles, double d) throws HiveException {
+        if (_quantiles.length < 3) {
             throw new HiveException(
                 "Length of `quantiles` should be greater than or equal to three but "
                         + _quantiles.length + ".");
-
-        int left = 0;
-        int right = _quantiles.length - 1;
-        int p = (left + right) / 2;
-        while (left + 1 != right) {
-            if (_quantiles[p] < target) {
-                left = p;
-                p = (left + right) / 2;
-            } else {
-                right = p;
-                p = (left + right) / 2;
-            }
         }
-        return p;
+
+        int res = Arrays.binarySearch(_quantiles, d);
+        return (res < 0) ? ~res - 1 : (res == 0) ? 0 : res - 1;
     }
 
     @Override
     public String getDisplayString(String[] children) {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         sb.append("feature_binning");
         sb.append("(");
         if (children.length > 0) {
