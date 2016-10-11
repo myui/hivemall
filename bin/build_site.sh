fffa6e30bda8357528be51c2264da9f0f668fc0d
myui <yuin405@gmail.com>
2016-10-11 13:56:47 +0900

    Fixed top page

---
+++ bin/build_site.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ "$HIVEMALL_HOME" == "" ]; then
+    if [ -e ../bin/${0##*/} ]; then
+	HIVEMALL_HOME=".."
+    elif [ -e ./bin/${0##*/} ]; then
+	HIVEMALL_HOME="."
+    else
+	echo "env HIVEMALL_HOME not defined"
+	exit 1
+    fi
+fi
+
+cd $HIVEMALL_HOME
+mvn clean site
+
+#
+# Run HTTP server on localhost
+#
+
+# ruby
+cd $HIVEMALL_HOME/target/site
+ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => "./", :Port => 8000).start'
+
+# python3
+#python -m http.server 8000
+
+# python2
+#python -m SimpleHTTPServer 8000
+
+
+
